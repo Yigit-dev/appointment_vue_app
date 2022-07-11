a085b605427230455b9d15b316e26ab63e4f0e7f
Yiğit Çakmak <yigitcakmak.dev@gmail.com>
2022-07-11 20:51:16 +0300

    Created Avatar Component

---
+++ src/components/Avatar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <img src="@/assets/yigit-light.jpg" :width="size" :height="size" alt="" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Avatar",
+  props: ["size"],
+};
+</script>
+
+<style scoped>
+img {
+  border-radius: 100%;
+  border: 2px solid var(--c-purple);
+  flex-shrink: 0;
+  object-fit: cover;
+}
+</style>
