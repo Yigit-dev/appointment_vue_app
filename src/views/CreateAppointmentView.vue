fb5a87c2e7e6dd06a4463e12decff5c71c07317b
Yiğit Çakmak <yigitcakmak.dev@gmail.com>
2022-07-19 17:42:05 +0300

    Create Appointment Page

--- src/views/CreateAppointmentView.vue
+++ src/views/CreateAppointmentView.vue
@@ -1,18 +1,71 @@
 <template>
   <section id="createAppointment" class="pageAnimation">
     <h1>Create Appointment</h1>
-    <CustomInput />
-
-    <CustomSelectBox
-      :options="['option1', 'option2', 'option3', 'option4']"
-      :default="'option1'"
-    />
+    <div class="wrap xl-2 lg-1">
+      <div class="col">
+        <form>
+          <p>Please enquire here.</p>
+          <div class="wrap xl-2 md-1 xl-gutter-24">
+            <div class="col">
+              <CustomInput label="First Name" />
+            </div>
+            <div class="col">
+              <CustomInput label="Last Name" />
+            </div>
+          </div>
+          <CustomInput label="Email" type="email" />
+          <CustomInput label="Phone" type="tel" />
+          <CustomSelectBox
+            label="Agent"
+            :options="this._agentName"
+            :default="'agent1'"
+          />
+          <Button color="var(--c-orange)">
+            <CustomText>Sent</CustomText>
+          </Button>
+        </form>
+      </div>
+      <div class="col">
+        <div>
+          <h1>Section 2</h1>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 <script>
 import CustomInput from "@/components/CustomInput.vue";
-import CustomSelectBox from "../components/CustomSelectBox.vue";
+import CustomSelectBox from "@/components/CustomSelectBox.vue";
+import Button from "@/components/Button.vue";
+import CustomText from "@/components/CustomText.vue";
+// import { mapMutations } from "vuex";
 export default {
-  components: { CustomInput, CustomSelectBox },
+  components: { CustomInput, CustomSelectBox, Button, CustomText },
+  computed: {},
+  async created() {
+    await this.$store.dispatch("loadAgents");
+  },
 };
 </script>
+
+<style lang="scss">
+form {
+  width: 90%;
+  background: var(--c-form);
+  border-radius: 16px;
+  margin: 20px 0px;
+  padding: 40px 20px 64px;
+  @media screen and (max-width: 929px) {
+    padding: 40px 10px 64px;
+    width: 100%;
+  }
+  label {
+    display: block;
+    padding-top: 24px;
+    padding-bottom: 12px;
+  }
+  button {
+    margin-top: 36px;
+  }
+}
+</style>
